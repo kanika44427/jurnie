5055fbefe4ba0dc4e4baa2e92217b726742ee189
kanika <kanikasethi04@gmail.com>
2018-11-21 01:32:00 +0530

    release

--- scripts/general/app.js
+++ scripts/general/app.js
@@ -20,6 +20,7 @@
             delete $httpProvider.defaults.headers.common['X-Requested-With'];
         }])
 		//.constant('ServerUrl', 'https://api.jurnie.com/api/v1/');
-	    .constant('ServerUrl', 'http://api.thejurnie.com/api/v1/');
+      
+	    .constant('ServerUrl', 'https://api.thejurnie.com/api/v1/');
 	// .constant('ServerUrl', 'http://localhost:3000/api/v1/');
 })();
