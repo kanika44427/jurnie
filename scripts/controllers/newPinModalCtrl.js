c9ef5e788d5085df95b38daf46d35177a1dc2c28
kanika <kanikasethi04@gmail.com>
2019-05-02 02:31:25 +0530

    prod

--- scripts/controllers/newPinModalCtrl.js
+++ scripts/controllers/newPinModalCtrl.js
@@ -105,7 +105,7 @@
 		    var today = new Date();
 		    var dd = today.getDate();
 		    var mm = today.getMonth() + 1;
-		    var yyyy = today.getFullYear();
+		    var yyyy = today.getFullYear().toString().substr(-2);
 		    if (dd < 10) {
 		        dd = '0' + dd;
 		    }
